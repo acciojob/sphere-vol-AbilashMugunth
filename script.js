a7728866745701d85f463b999c3ed250c90c972f
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 19:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-let volumeInput = document.getElementById("volume");
 let calculate = document.getElementById("calculate");
+
 
 function volume_sphere() {
   let radiusInput = document.getElementById("radius").value;
-
-  const volume = (4 / 3) * Math.PI * Math.pow(radiusInput, 3);
-  console.log(radiusInput);
-  console.log(volume);
+  let volumeInput = document.getElementById("volume");
+  let volume = (4 / 3) * Math.PI * Math.pow(radiusInput, 3);
   volumeInput.value = volume;
 }
 
